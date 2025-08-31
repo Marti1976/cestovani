731df36126a4ddc5307354654ff47b58c8692325
Marti1976 <125206366+Marti1976@users.noreply.github.com>
2025-08-31 21:26:49 +0200

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 export const IconMapPin: React.FC = () => (
@@ -148,5 +147,17 @@ export const IconCancel: React.FC = () => (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <line x1="18" y1="6" x2="6" y2="18"></line>
     <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+);
+
+export const IconFlagCheckered: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M14.4 6H20v11h-5.6L12 15.4V10l2.4-4M13 2L4 3.76V11l9 2-9 2v7.24L13 22l9-10-9-10Z"/>
+    </svg>
+);
+
+export const IconChevronDown: React.FC<{ className?: string }> = ({ className }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={className || "h-6 w-6"} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <polyline points="6 9 12 15 18 9"></polyline>
   </svg>
 );
